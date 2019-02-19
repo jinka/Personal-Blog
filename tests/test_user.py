bc3082122af58d4b50b260e97abd07729ff094f2
jinka <voiphandy@gmail.com>
2019-02-19 08:36:57 +0300

    Add unitest for User model

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,2 +1,8 @@
 from app.models import User, Post, Comment
 from app import db
+
+def setUp(self):
+        self.user_Daud = User(username = 'Daud',password = 'password', email = 'daudi@ms.com')
+        self.new_review = User(user_id=12345,username='jinka'user = self.user_Daud )
+
+F
