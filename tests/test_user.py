fe038f96a91958ddfd9ff14e75bd14333c41f200
jinka <voiphandy@gmail.com>
2019-02-19 08:40:42 +0300

    Add teardown and test_check_variables into User test

--- tests/test_user.py
+++ tests/test_user.py
@@ -5,4 +5,10 @@ def setUp(self):
         self.user_Daud = User(username = 'Daud',password = 'password', email = 'daudi@ms.com')
         self.new_review = User(user_id=12345,username='jinka'user = self.user_Daud )
 
-F
+def tearDown(self):
+        Review.query.delete()
+        User.query.delete()
+
+def test_check_instance_variables(self):
+        self.assertEquals(self.new_user_name,'ali')
+        self.assertEquals(self.new_[password],'12345')
