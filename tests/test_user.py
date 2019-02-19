8d6a7e17ed25500490b55ab55ac0eabc1c1eed84
jinka <voiphandy@gmail.com>
2019-02-19 09:01:52 +0300

    Added test check instance variables

--- tests/test_user.py
+++ tests/test_user.py
@@ -6,9 +6,20 @@ def setUp(self):
         self.new_review = User(user_id=12345,username='jinka'user = self.user_Daud )
 
 def tearDown(self):
-        Review.query.delete()
         User.query.delete()
 
 def test_check_instance_variables(self):
-        self.assertEquals(self.new_user_name,'ali')
-        self.assertEquals(self.new_[password],'12345')
+        self.assertEquals(self.new_id,12345)
+        self.assertEquals(self.new_full_name,'Mike simon')
+        self.assertEquals(self.new_user_name,'Mike')
+        self.assertEquals(self.new_email,'daud@gmail.com')
+        self.assertEquals(self.phone,'077777777')
+        self.assertEquals(self.role_id,'111')
+        self.assertEquals(self.bio,'aaaa')
+        self.assertEquals(self.phone,'077777777')
+        self.assertEquals(self.profile_picture,'/static/image/1.jpg')
+        self.assertEquals(self.pass_secure,'ccccc')
+        self.assertEquals(self.password_hash,'ccccc')
+        
+
+
